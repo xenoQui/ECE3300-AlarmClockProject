23a5af767d36686b469eb0feda7d3ba6c44ef769
jluuu <143465751+jluuu@users.noreply.github.com>
2023-12-10 16:42:08 -0800

    Update alarm_player.v

--- AlarmClock/v2.4/sources_1/new/alarm_player.v
+++ AlarmClock/v2.4/sources_1/new/alarm_player.v
@@ -23,7 +23,6 @@
 module alarm_player(
                     input clk,
                     input rst,
-                    input stop,
                     input player_en,
                     output alarm_r, alarm_g, alarm_b,             
                     output reg audio_out, aud_sd             
@@ -50,32 +49,28 @@ module alarm_player(
                     .PWM_b(alarm_b) // Blue LED
                 );
     
-    always @ (posedge clk) 
+    always@(posedge clk) 
       begin
         if (~player_en | rst)
             begin
 
                 counter <= 0;
                 time1 <= 0;
-                audio_out<=1;
+                audio_out <= 1;
             end
 
-        if(stop == 1) 
-                aud_sd = 0;
-        else 
-            aud_sd = 1;
-            
+        aud_sd = 1;  
         counter <= counter + 1; 
         time1<= time1+1;
         
-        if( counter >= notePeriod) 
+        if(counter >= notePeriod) 
            begin
-                counter <=0;
+                counter <= 0;
                 audio_out <= ~audio_out ; 
            end //toggle audio output 
-        if( time1 >= noteTime) 
+        if(time1 >= noteTime) 
             begin
-                time1 <=0;
+                time1 <= 0;
                 number <= number + 1; 
             end  //play next note
 
@@ -84,6 +79,6 @@ module alarm_player(
       always @(duration)
       begin
        noteTime = (duration * clockFrequency/8);  //number of   FPGA clock periods in one note.
-       end
+      end
       
 endmodule
